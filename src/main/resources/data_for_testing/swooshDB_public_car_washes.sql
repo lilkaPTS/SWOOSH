dbd2957669d7f2249c0388ee8e06236ed05fde56
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-03-20 14:05:56 +0300

    Init data version date: 20.03

--- src/main/resources/data_for_testing/swooshDB_public_car_washes.sql
+++ src/main/resources/data_for_testing/swooshDB_public_car_washes.sql
@@ -1,1 +1,2 @@
 INSERT INTO public.car_washes (car_wash_id, location) VALUES (1, 'Полтавская 47');
+INSERT INTO public.car_washes (car_wash_id, location) VALUES (4, 'Республиканская 37');
--- src/main/resources/data_for_testing/swooshDB_public_employees.sql
+++ src/main/resources/data_for_testing/swooshDB_public_employees.sql
@@ -1,1 +1,2 @@
-INSERT INTO public.employees (employee_id, name, car_wash_id) VALUES (1, 'Иванов Иван Иванович', 1);
+INSERT INTO public.employees (employee_id, name, car_wash_id, passport_data) VALUES (2, 'Петров Пётр Петрович', 1, '6786758754');
+INSERT INTO public.employees (employee_id, name, car_wash_id, passport_data) VALUES (1, 'Иванов Иван Иванович', 1, '4362375734');
